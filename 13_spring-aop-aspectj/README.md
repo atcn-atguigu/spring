524fab72dc1b707dfd1a39f69833de2fc5e4c09d
BengiOnMAC <356844587@qq.com>
2022-06-07 18:00:56 +0800

    Spring AOP - AOP操作（AspectJ注解）- @Pointcut提取公共部分 @Order设置多个代理类之间优先顺序

--- 13_spring-aop-aspectj/README.md
+++ 13_spring-aop-aspectj/README.md
@@ -220,6 +220,72 @@ UserProxy.after()...后置通知（最终通知）
 java.lang.ArithmeticException: / by zero
 ```
 
+5、相同的切入点抽取使用@Pointcut
+
+```java
+// 相同的切入点抽取
+@Pointcut(value = "execution(* com.atguigu.spring5.aop.annotation.User.add(..))")
+public void pointcutAddMethod() {
+}
+
+//环绕通知 - 之前之后都执行
+@Around(value = "pointcutAddMethod()")
+public void around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+  System.out.println("UserProxy.around()...环绕之前");
+
+  //被增强的方法
+  proceedingJoinPoint.proceed();  // 这里会捕获异常，如果此处被@AfterThrowing拦住，则不执行该环绕通知方法后的所有代码
+
+  System.out.println("UserProxy.around()...环绕之后");
+}
+
+//前置通知
+@Before(value = "pointcutAddMethod()")
+public void before() {
+  System.out.println("UserProxy.before()...前置通知");
+}
+```
+
+6、有多个增强类对同一方法进行增强，设置增强类优先级别
+	1）在增强类上添加注解@Order(数字类型值)，数字类型值越小优先级越高
+
+```java
+@Component
+@Aspect // 生成代理对象
+@Order(1) // 在增强类上添加注解@Order(数字类型值)，数字类型值越小优先级越高
+public class UserProxy {
+  
+@Component
+@Aspect // 生成代理对象
+@Order(2) // 在增强类上添加注解@Order(数字类型值)，数字类型值越小优先级越高
+public class UserProxy2 {
+  
+@Component
+@Aspect // 生成代理对象
+@Order(3) // 在增强类上添加注解@Order(数字类型值)，数字类型值越小优先级越高
+public class UserProxy3 {
+```
+
+​	Output:
+
+```plain/text
+UserProxy.around()...环绕之前
+UserProxy.before()...前置通知
+UserProxy2.around()...环绕之前 -- @Order(2)
+UserProxy2.before()...前置通知 -- @Order(2)
+UserProxy3.around()...环绕之前 -- @Order(3)
+UserProxy3.before()...前置通知 -- @Order(3)
+User.add()...
+UserProxy3.afterReturning()...后置返回通知（返回结果执行） -- @Order(3)
+UserProxy3.after()...后置通知（最终通知） -- @Order(3)
+UserProxy3.around()...环绕之后 -- @Order(3)
+UserProxy2.afterReturning()...后置返回通知（返回结果执行） -- @Order(2)
+UserProxy2.after()...后置通知（最终通知） -- @Order(2)
+UserProxy2.around()...环绕之后 -- @Order(2)
+UserProxy.afterReturning()...后置返回通知（返回结果执行）
+UserProxy.after()...后置通知（最终通知）
+UserProxy.around()...环绕之后
+```
 
 
-### AOP操作（AspectJ配置文件）
+
--- 13_spring-aop-aspectj/src/main/java/com/atguigu/spring5/aop/annotation/UserProxy3.java
+++ 13_spring-aop-aspectj/src/main/java/com/atguigu/spring5/aop/annotation/UserProxy3.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Component;
 //增强的类
 @Component
 @Aspect // 生成代理对象
-@Order(1) // 在增强类上添加注解@Order(数字类型值)，数字类型值越小优先级越高
-public class UserProxy {
+@Order(3) // 在增强类上添加注解@Order(数字类型值)，数字类型值越小优先级越高
+public class UserProxy3 {
 
     // 相同的切入点抽取
     @Pointcut(value = "execution(* com.atguigu.spring5.aop.annotation.User.add(..))")
@@ -19,35 +19,35 @@ public class UserProxy {
     //环绕通知 - 之前之后都执行
     @Around(value = "pointcutAddMethod()")
     public void around(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        System.out.println("UserProxy.around()...环绕之前");
+        System.out.println("UserProxy3.around()...环绕之前 -- @Order(3)");
 
         //被增强的方法
         proceedingJoinPoint.proceed();  // 这里会捕获异常，如果此处被@AfterThrowing拦住，则不执行该环绕通知方法后的所有代码
 
-        System.out.println("UserProxy.around()...环绕之后");
+        System.out.println("UserProxy3.around()...环绕之后 -- @Order(3)");
     }
 
     //前置通知
     @Before(value = "pointcutAddMethod()")
     public void before() {
-        System.out.println("UserProxy.before()...前置通知");
+        System.out.println("UserProxy3.before()...前置通知 -- @Order(3)");
     }
 
     //后置通知（最终通知）
     @After(value = "pointcutAddMethod()")
     public void after() {
-        System.out.println("UserProxy.after()...后置通知（最终通知）");
+        System.out.println("UserProxy3.after()...后置通知（最终通知） -- @Order(3)");
     }
 
     //后置返回通知（返回结果执行）
     @AfterReturning(value = "pointcutAddMethod()")
     public void afterReturning() {
-        System.out.println("UserProxy.afterReturning()...后置返回通知（返回结果执行）");
+        System.out.println("UserProxy3.afterReturning()...后置返回通知（返回结果执行） -- @Order(3)");
     }
 
     //异常通知
     @AfterThrowing(value = "pointcutAddMethod()")
     public void afterThrowing() {
-        System.out.println("UserProxy.afterThrowing()...异常通知");
+        System.out.println("UserProxy3.afterThrowing()...异常通知 -- @Order(3)");
     }
 }
